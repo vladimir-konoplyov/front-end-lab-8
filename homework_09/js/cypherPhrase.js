f2da8f42578961e7492af5818ab46089ceab8b12
Volodimir Konoplyov <volodimir.konoplyov@gmail.com>
2018-02-19 22:06:25 +0200

    Added cypherPhrase.js

---
+++ homework_09/js/cypherPhrase.js
@@ -0,0 +1,12 @@
+function cypherPhrase(obj, str) {
+    let arr = [] = str.split('');
+    let newString = getTransformedArray(arr, function(elem) {
+    for (let key in obj) {
+      if (elem === key) {
+        return obj[key];
+      } 
+    }
+    return elem;
+  });
+  return newString.join(''); 
+}
