d54238759b90c9887e788427a9515c9e0c195e47
Volodimir Konoplyov <volodimir.konoplyov@gmail.com>
2018-02-24 00:35:02 +0200

    Task 1 added debounce function

---
+++ homework_10/task1.js
@@ -0,0 +1,33 @@
+function debounce(callBack, delay) {
+    let timeoutId = null;
+    return function() {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(callBack, delay);
+    };
+}
+
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
